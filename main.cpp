cf931438850190b5959bd098d25b37666aec8942
Tihon Vatkin <fpm.vatkin@bsu.by>
2024-11-16 14:47:14 +0300

    AAAAAAAAAA

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int main() {
     student stud3 = student (stud1);
     std::cout << stud1 << '\n' << stud2 << '\n' << stud3 << '\n';
     int marks[4] = {4, 6, 8, 3};
-    studentAfterSession1 stud4 = studentAfterSession1("Чмоня", 3 , 52, marks);
+    int marks1[5] = {9, 8, 7, 6, 5};
+    studentAfterSession2 stud4 = studentAfterSession2("Чмоня", 3 , 52, marks, marks1);
     std::cout << stud4 << "\n" << stud4.avarageScore() << '\n';
-    studentAfterSession1 stud5 = studentAfterSession1(stud4);
-    std::cout << stud4;
 }
--- students.h
+++ students.h
@@ -7,20 +7,6 @@
 #include <cstring>
 #include <array>
 #include <random>
-// int generateUniqueId() {
-//   std::random_device rd;
-//   std::mt19937 generator(rd());
-//   std::uniform_int_distribution<> distribution(100000000, 999999999);
-//   int uniqueId = distribution(generator);
-//   return uniqueId;
-// }
-// int generateRecord() {
-//   std::random_device rd;
-//   std::mt19937 generator(rd());
-//   std::uniform_int_distribution<> distribution(0, 100);
-//   int uniqueRecord = distribution(generator);
-//   return uniqueRecord;
-// }
 class student {
     protected:
     char name[100];
@@ -114,7 +100,52 @@ class studentAfterSession1 : public student {
         return avarageScore / 4;
     }
 };
-
+class studentAfterSession2 : public studentAfterSession1 {
+    protected:
+    int markSession2[5];
+    public:
+    studentAfterSession2() = delete;
+    studentAfterSession2(char* _name, int _course, int _group, int* marks1, int* marks2) : studentAfterSession1(_name, _course, _group, marks1) {
+        for (int i = 0; i < 5; i++) {
+            markSession2[i] = marks2[i];
+        }
+    }
+    studentAfterSession2(const studentAfterSession2 &other) : studentAfterSession1(other) {
+        for (int i = 0; i < 5; i++) {
+            markSession2[i] = other.markSession2[i];
+        }
+    }
+    int* getMarks2() {
+        return markSession2;
+    }
+    int getMarks2(int position) {
+        return markSession2[position];
+    }
+    void printMarks2 (int* marks) {
+        std::cout << "Массив оценок за первый сем. ";
+        for (int i = 0; i < 5; i++){
+            std::cout << marks[i] << ' ';
+        }
+    }
+    void setMarks2(int* marks) {
+        for (int i = 0; i < 5; i++) {
+            markSession2[i] = marks[i];
+        }
+    }
+    void setMarks2(int mark, int position) {
+        markSession2[position] = mark;
+    }
+    double avarageScore() override {
+        double avarageScore;
+        for (int i = 0; i < 4; i++) {
+            avarageScore += markSession1[i];
+        }
+        for (int i = 0; i < 5; i++) {
+            avarageScore += markSession2[i];
+        }
+        return avarageScore / 9;
+    }
+};
 std::ostream& operator << (std::ostream &os, student &person)
 {   os << "ID: " << std::hex << person.getId() << '\n';
     os << "Имя студента: " << person.getName() << '\n';
@@ -130,8 +161,25 @@ std::ostream& operator << (std::ostream &os, studentAfterSession1 &person)
     os << "В группе: " << person.getGroup() << '\n';
     os << "Номер зачетки: " << person.getRecordBook() << '\n';
     os << "Массив оценок за первый сем. ";
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         os << person.getMarks1()[i] << ' ';
+    }
+    return os;
+}
+std::ostream& operator << (std::ostream &os, studentAfterSession2 &person)
+{   os << "ID: " << std::hex << person.getId() << '\n';
+    os << "Имя студента: " << person.getName() << '\n';
+    os << "Учиться на курсе: " << person.getCourse() << '\n';
+    os << "В группе: " << person.getGroup() << '\n';
+    os << "Номер зачетки: " << person.getRecordBook() << '\n';
+    os << "Массив оценок за первый сем. ";
+    for (int i = 0; i < 4; i++) {
+        os << person.getMarks1()[i] << ' ';
+    }
+    os << '\n';
+    os << "Массив оценок за второй сем. ";
+    for (int i = 0; i < 5; i++) {
+        os << person.getMarks2()[i] << ' ';
     }
     return os;
 }
